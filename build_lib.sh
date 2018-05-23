e6d19dc6b6879db8b410da2bf9970e51c2767b15
Stefan Reinhard <stefan.reinhard@bitforge.ch>
2018-05-23 15:55:53 +0200

    Add automated build

---
+++ build_lib.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+pod install --project-directory=google-maps-ios-utils/workspace
+
+mkdir -p build
+
+xcodebuild \
+	-configuration Release \
+	-sdk iphoneos \
+	-scheme GoogleMapsUtils \
+	-workspace google-maps-ios-utils/workspace/GoogleMapsUtils.xcworkspace \
+	-derivedDataPath build
+
+xcodebuild \
+	-configuration Release \
+	-sdk iphonesimulator \
+	-scheme GoogleMapsUtils \
+	-workspace google-maps-ios-utils/workspace/GoogleMapsUtils.xcworkspace \
+	-derivedDataPath build
+
+lipo -create \
+	build/Build/Products/Release-iphoneos/libGoogleMapsUtils.a \
+	build/Build/Products/Release-iphonesimulator/libGoogleMapsUtils.a \
+	-output GoogleMapsUtility-iOS-Binding/libGoogleMapsUtils.a
